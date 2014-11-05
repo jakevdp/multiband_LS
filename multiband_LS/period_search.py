d385b78d08f15ca5e446304b42d106eb756f2ac3
Jake VanderPlas <jakevdp@gmail.com>
2014-11-05 11:28:38 -0800

    add flexibility to period search

--- multiband_LS/period_search.py
+++ multiband_LS/period_search.py
@@ -1,5 +1,6 @@
 import numpy as np
-from astroML.time_series import lomb_scargle
+from astroML.time_series import lomb_scargle as lomb_scargle_astroML
+from .lomb_scargle import lomb_scargle
 
 
 def _single_curve(t, y, dy, i):
@@ -12,8 +13,15 @@ def _single_curve(t, y, dy, i):
     return (t[good], y[good], dy[good])    
 
 
-def period_search(t, y, dy, pmin=0.2, pmax=1.0, N=50000):
+def period_search(t, y, dy, pmin=0.2, pmax=1.0, N=50000,
+                  LS_func=None, LS_kwargs=None):
     """Search periods band-by-band"""
+
+    if LS_func is None:
+        LS_func = lomb_scargle_astroML
+    if LS_kwargs is None:
+        LS_kwargs = {}
+
     # do a coarse-grained zoom for the first pass
     data = [_single_curve(t, y, dy, i) for i in range(5)]
     period = np.linspace(pmin, pmax, N)
@@ -24,7 +32,7 @@ def period_search(t, y, dy, pmin=0.2, pmax=1.0, N=50000):
         # shift period for greater coverage
         omega = 2 * np.pi / (period + 0.2 * i * dp)
         t, y, dy = data[i]
-        P = lomb_scargle(t, y, dy, omega)
+        P = LS_func(t, y, dy, omega, **LS_kwargs)
         p_best.append(period[np.argmax(P)])
 
     # now zoom-in around the best periods
@@ -38,7 +46,7 @@ def period_search(t, y, dy, pmin=0.2, pmax=1.0, N=50000):
     p_best = []
     for i, filt in enumerate('ugriz'):
         t, y, dy = data[i]
-        P = lomb_scargle(t, y, dy, omega)
+        P = LS_func(t, y, dy, omega, **LS_kwargs)
         p_best.append(period[np.argmax(P)])
 
     return np.array(p_best)
