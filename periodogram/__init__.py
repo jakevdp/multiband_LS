88fcdca5c5f8560bd5cbe909321267464cef6b73
Jake VanderPlas <jakevdp@gmail.com>
2015-01-12 11:08:02 -0800

    add multiband model refactors

--- periodogram/__init__.py
+++ periodogram/__init__.py
@@ -1,2 +1,6 @@
+"""General Periodic Modeling for Astronomical Time Series"""
+
 from .lomb_scargle import LombScargle, LombScargleAstroML
+from .lomb_scargle_multiband import (LombScargleMultiband,
+                                     LombScargleMultibandFast)
 from .supersmoother import SuperSmoother
