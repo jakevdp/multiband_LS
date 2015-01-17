30d5af751952755ab1ee267b147ce64bfb98bdeb
Jake VanderPlas <jakevdp@gmail.com>
2015-01-16 17:23:01 -0800

    clean up parallel computation code

--- writeup/figures/compute_rrlyrae_periods.py
+++ writeup/figures/compute_rrlyrae_periods.py
@@ -10,10 +10,65 @@ from multiband_LS.data import fetch_light_curves
 from multiband_LS.memoize import CacheResults
 
 
+def best_period_Multiband(lcid, rrlyrae,
+                          Nterms_base=1, Nterms_band=0):
+    """Best period using the Multi-band Lomb-Scargle algorithm.
+
+    Compute the best period for the given light curve, using the multiband
+    model with the given Nbase and Nband
+    """
+    t, y, dy, filts = rrlyrae.get_lightcurve(lcid, return_1d=True)
+    ls = multiband_LS.LombScargleMultiband(Nterms_base=Nterms_base,
+                                           Nterms_band=Nterms_band)
+    ls.optimizer.period_range = (0.2, 1.2)
+    ls.fit(t, y, dy, filts)
+    return ls.best_period
+
+
+def periods_Multiband(lcids, Nterms_base=1, Nterms_band=0,
+                      func=best_period_Multiband):
+    """Compute and cache best periods for the given light curve ids"""
+    cachedir = 'results_multiband_{0}_{1}'.format(Nterms_base, Nterms_band)
+    cache = CacheResults(cachedir, verbose=True)
+    rrlyrae = fetch_light_curves()
+    results = cache.call_iter(func, lcids,
+                              args=(rrlyrae, Nterms_base, Nterms_band))
+    return numpy.asarray(results)
+
+
+def best_period_SuperSmoother(lcid, rrlyrae, filt='g'):
+    """Best period using the SuperSmoother algorithm
+
+    Compute the best period for the given light curve, using data from the
+    given filter.
+    """
+    t, y, dy, filts = rrlyrae.get_lightcurve(lcid, return_1d=True)
+    t, y, dy = (x[filts == filt] for x in (t, y, dy))
+    ssm = multiband_LS.SuperSmoother()
+    ssm.optimizer.period_range = (0.2, 1.2)
+    ssm.fit(t, y, dy)
+    return ssm.best_period
+
+
+def periods_SuperSmoother(lcids, filt='g', func=best_period_SuperSmoother):
+    """Compute and cache best periods for the given light curve ids"""
+    cachedir = 'results_supersmoother_{0}'.format(filt)
+    cache = CacheResults(cachedir, verbose=True)
+    rrlyrae = fetch_light_curves()
+    results = cache.call_iter(func, lcids, args=(rrlyrae, filt))
+    return numpy.asarray(results)
+
+
 def parallelize(func, *client_args, **client_kwargs):
+    """Parallelize a function"""
     @contextlib.wraps(func)
     def wrapper(lcids, *args, **kwargs):
-        client = parallel.Client(*client_args, **client_kwargs)
+        try:
+            client = parallel.Client(*client_args, **client_kwargs)
+        except FileNotFoundError:
+            raise ValueError("Error accessing IPython Parallel clients. "
+                             "Did you run ``ipcluster start``?")
+            
         print("client ids:", client.ids)
 
         dview = client.direct_view()
@@ -43,45 +98,9 @@ def parallelize(func, *client_args, **client_kwargs):
     return wrapper
 
 
-def best_period_Multiband(lcid, rrlyrae,
-                          Nterms_base=1, Nterms_band=0):
-    t, y, dy, filts = rrlyrae.get_lightcurve(lcid, return_1d=True)
-    ls = multiband_LS.LombScargleMultiband(Nterms_base=Nterms_base,
-                                           Nterms_band=Nterms_band)
-    ls.optimizer.period_range = (0.2, 1.2)
-    ls.fit(t, y, dy, filts)
-    return ls.best_period
-
-
-def periods_Multiband(lcids, Nterms_base=1, Nterms_band=0,
-                      func=best_period_Multiband):
-    cachedir = 'results_multiband_{0}_{1}'.format(Nterms_base, Nterms_band)
-    cache = CacheResults(cachedir, verbose=True)
-    rrlyrae = fetch_light_curves()
-    results = cache.call_iter(func, lcids,
-                              args=(rrlyrae, Nterms_base, Nterms_band))
-    return numpy.asarray(results)
-
-
-def best_period_SuperSmoother(lcid, rrlyrae, filt='g'):
-    t, y, dy, filts = rrlyrae.get_lightcurve(lcid, return_1d=True)
-    t, y, dy = (x[filts == filt] for x in (t, y, dy))
-    ssm = multiband_LS.SuperSmoother()
-    ssm.optimizer.period_range = (0.2, 1.2)
-    ssm.fit(t, y, dy)
-    return ssm.best_period
-
-
-def periods_SuperSmoother(lcids, filt='g', func=best_period_SuperSmoother):
-    cachedir = 'results_supersmoother_{0}'.format(filt)
-    cache = CacheResults(cachedir, verbose=True)
-    rrlyrae = fetch_light_curves()
-    results = cache.call_iter(func, lcids, args=(rrlyrae, filt))
-    return numpy.asarray(results)
-
-
 periods_Multiband_parallel = parallelize(periods_Multiband)
 periods_SuperSmoother_parallel = parallelize(periods_SuperSmoother)
+
 
 if __name__ == '__main__':
     rrlyrae = fetch_light_curves()
